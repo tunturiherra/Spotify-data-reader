af26946ca888f3d068f1c3b77cd0064394a5d458
Sammeli Näkkäläjärvi <nnakkala@edu.lapinamk.fi>
2023-12-10 22:05:37 +0200

    GDPR velvoittaa yrityksiä luovuttamaan käyttäjän pyynnöstä kaikki hänestä kerätyn datan. Usein data on JSON-muodossa, mikä ei välttämättä ole helpoin tapa selata tietoa käyttäjän näkökulmasta. Tämän ohjelman avulla voidaan hakea kaikki kuunnellut laulun helposti ymmärrettävässä muodossa.

---
+++ spotify_data.py
@@ -0,0 +1,37 @@
+import json
+import datetime
+
+
+# muunnetaan tällä funktiolla Spotifylta tuleva aikaleima helpommin luettavaan muotoon
+def get_year(timestamp):
+    date_object = datetime.datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%SZ")
+    return date_object.year
+
+# Jos windowsilla yrittää kopioida tiedoston sijaintia, niin tulee lainausmerkit mukaan.
+# käytetään replacea, jotta käyttäjän ei tarvitse erikseen siivota inputista lainausmerkkejä.
+filepath = input("Anna tiedoston sijainti: \n").replace('"',"")
+
+# haetaan tiedot json-tiedostosta ja lisätään se omaksi muuttujaksi
+with open(filepath, 'r', encoding='utf-8') as file:
+    data = json.load(file)
+
+    # kysytään käyttäjältä haluttua vuotta
+    listen_year = int(input("Anna haluttu vuosi:\n"))
+
+    # jotta ohjelma ei tulostaisi miljoonaa kertaa virheviestiä, otetaan vain toteutuneet arvot.
+    found_songs = False
+
+    for song in data:
+        # tarkistetaan ja tulostetaan tiedot vain, jos vuosi täsmää
+        if get_year(song["ts"]) == listen_year:
+            artist = song["master_metadata_album_artist_name"]
+            track = song["master_metadata_track_name"]
+            year = get_year(song["ts"])
+            device = song["platform"]
+
+            # tulostetaan yhdelle riville per dictionary
+            print(f"{artist}: {track}, kuunneltu vuonna {year} laitteelta {device}")
+            found_songs = True
+
+    if not found_songs:
+        print(f"Ei löytynyt tietoja vuodelta {listen_year}.")
